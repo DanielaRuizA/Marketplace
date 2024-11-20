5cdeebcca3284100223acd05c6464da74aff0779
DanielaRuizA <daruiz1218@gmail.com>
2024-11-19 19:54:42 -0500

    first commit

---
+++ app/models/product.rb
@@ -0,0 +1,5 @@
+class Product < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 4, maximum: 20 }
+  validates :description, presence: true, length: { minimum: 4, maximum: 60 }
+  validates :price, numericality: { greater_than: 900 }
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/products/new", to: "products#new", as: :new_product
+  get "/products/:id/edit", to: "products#edit", as: :edit_product
+  patch "/products/:id", to: "products#update"
+  delete "/products/:id", to: "products#destroy"
+  get "/products/:id", to: "products#show", as: :product
+  post "/products", to: "products#create", as: :products
+
+  # Defines the root path route ("/")
+  root "products#index"
+end
