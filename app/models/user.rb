6801275826502cd833e71db557d66d1e7defdd39
DanielaRuizA <daruiz1218@gmail.com>
2024-11-21 08:52:58 -0500

    add validation to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
-        has_many :products, dependent: :destroy
+  has_many :products, dependent: :destroy
+
+  validates :name, presence: { message: "Can't be blank" },
+  length: { minimum: 2, maximum: 50, message: "Must be between 2 and 50 characters" }
+validates :address, presence: { message: "Can't be blank" },
+  length: { minimum: 5, maximum: 100, message: "Must be between 5 and 100 characters" }
+validates :phone, presence: { message: "Can't be blank" },
+  length: { minimum: 9, maximum: 15, message: "Must be between 9 and 15 digits" }
 end
